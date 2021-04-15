30e05a848881c41d4bf8904100162d1386aedbad
edelmutic <edelmutic@gmail.com>
2021-04-15 17:45:14 +0300

    init

---
+++ Title.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledTitle = styled.h1`
+color: white
+`
+
+const Title = () => {
+    return (
+        <div>
+            Hi
+        </div>
+    )
+}
+
+export default Title
